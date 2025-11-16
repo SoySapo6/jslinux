4e7355df07cfa5e382a2dd57284b75893439f093
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-11-16 18:39:00 -0500

    Update jslinux.js

--- jslinux.js
+++ jslinux.js
@@ -107,7 +107,7 @@ function loadbinary(url, slot, save_local) {
             data = req.responseText;
           }
           binaries[slot] = data;
-
+          
           if (save_local && data instanceof ArrayBuffer) {
               const transaction = db.transaction(["files"], "readwrite");
               const objectStore = transaction.objectStore("files");
@@ -231,23 +231,6 @@ function save_root_bin() {
 
     request.onerror = (event) => {
         console.error("Could not save root.bin to IndexedDB:", event.target.error);
-    };
-}
-
-function save_root_bin() {
-    var ramdisk = pc.get_ramdisk_image();
-    const transaction = db.transaction(["files"], "readwrite");
-    const objectStore = transaction.objectStore("files");
-    const request = objectStore.put({ name: "root.bin", data: ramdisk });
-
-    request.onsuccess = () => {
-        console.log("root.bin saved to IndexedDB");
-        alert("Saved!");
-    };
-
-    request.onerror = (event) => {
-        console.error("Could not save root.bin to IndexedDB:", event.target.error);
-        alert("Could not save: " + event.target.error);
     };
 }
 
